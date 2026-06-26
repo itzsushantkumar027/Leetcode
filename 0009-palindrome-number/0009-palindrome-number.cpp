bcbde8c26187a792982076e425245d6a602c3662
itzsushantkumar027 <itzsushantkumar@gmail.com>
2026-06-26 20:21:56 +0530

    Time: 3 ms (45.22%), Space: 8.5 MB (92.25%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long reverse = 0;
+        int a=x;
+        while(x>0){
+            reverse=(reverse*10)+(x%10);
+            x /= 10;
+        }
+        return reverse == a;
+    }
+};
